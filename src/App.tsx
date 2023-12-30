3a1d764e02d36effcb189779e4b83d2f415842db
Tyler O'Briant <tylerobriant@gmail.com>
2023-12-29 19:08:30 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
-import { For, createSignal } from "solid-js";
+import { For, createSignal, Accessor, createEffect } from "solid-js";
+import { createLocalStore } from "./utils";
+
 import "./App.css";
 
+type Todo = {
+	description: string;
+	completed: boolean;
+};
+
 function App() {
-	const [count, setCount] = createSignal<number>(0);
+	const [todos, setTodos] = createSignal<Todo[]>([
+		{ description: "hi", completed: false },
+		{ description: "tyler", completed: true },
+	]);
+
+	const toggleTodo = (index: number) => {
+		setTodos((t) => {
+			const updatedTodos = [...t];
+
+			updatedTodos[index] = {
+				...updatedTodos[index],
+				completed: !updatedTodos[index].completed,
+			};
+
+			return updatedTodos;
+		});
+	};
+
+	const doSideEffect = (prev: Todo[]) => {
+		console.log(prev);
+	};
+
+	createEffect(() => doSideEffect(todos()));
 
 	return (
-		<>
-			{count}
-			<button
-				onClick={(e) => {
-					setCount((c) => c + 1);
-				}}
-			>
-				MORE
-			</button>
-		</>
+		<For each={todos()}>
+			{(todo, i) => {
+				console.log(i);
+				return (
+					<div>
+						{i()}-{todo.description}-{String(todo.completed)}
+						<button
+							onClick={(e) => {
+								e.preventDefault();
+								toggleTodo(i());
+							}}
+						>
+							Toggle
+						</button>
+					</div>
+				);
+			}}
+		</For>
 	);
 }
 
